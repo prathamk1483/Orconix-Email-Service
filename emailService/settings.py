e0b37c02e48f213013d4ef107f28a34320efd195
Pratham Kubetkar <kubetkarpratham14@gmail.com>
2026-01-03 22:39:45 +0530

    Allow cors to vercel app

--- emailService/settings.py
+++ emailService/settings.py
@@ -53,7 +53,8 @@ MIDDLEWARE = [
 ]
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "http://127.0.0.1:5173"
+    "http://127.0.0.1:5173",
+    "https://archify-backend.verce.app"
 ]
 ROOT_URLCONF = 'emailService.urls'
 
